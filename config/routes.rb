e5f543006357d2e473057809c0e6cfa33e7a5453
Tobias Schmidt <t.schmidt@rubidat.de>
2017-06-26 11:57:18 +0200

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+  resources :shareables, only: [:new, :create, :edit, :update, :destroy] do
+    collection do
+      put :add_multiple
+      delete :remove_multiple
+    end
+  end
 end
